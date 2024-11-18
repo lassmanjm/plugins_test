900ea4e4042f1dea98a0c6418bed99876f9eeec8
lassmanjm <lassmanjm@gmail.com>
2024-11-18 14:55:28 -0500

    Add vscode colorscheme to ui

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -41,4 +41,13 @@ return {
 			
 		end,
 	},
+	{
+		"Mofiqul/vscode.nvim",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		init = function()
+			vim.cmd.colorscheme("vscode")
+
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
 }
