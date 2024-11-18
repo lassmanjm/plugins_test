3413926d5010a28d6619db66fd80f1e5d56d0825
lassmanjm <lassmanjm@gmail.com>
2024-11-18 14:58:25 -0500

    change init -> config in loading vs code colorscheme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -44,7 +44,7 @@ return {
 	{
 		"Mofiqul/vscode.nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
-		init = function()
+		config = function()
 			vim.cmd.colorscheme("vscode")
 
 			vim.cmd.hi("Comment gui=none")
