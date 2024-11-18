529359b9af0093a674a63523bab3548af159cd51
Josh Lassman <lassmanjm@gmail.com>
2024-11-18 14:12:01 -0500

    Wrap keymaps and options in function so it can be imported by lazy

--- lua/plugins/keymaps_and_options.lua
+++ lua/plugins/keymaps_and_options.lua
@@ -1,131 +1,137 @@
-vim.cmd("source ~/.vimrc")
+local M ={}
 
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+function M.setup()
+    vim.cmd("source ~/.vimrc")
 
--- Keymappings for bar bar to switch and close buffers
-vim.keymap.set("n", "<leader>j", "<cmd>BufferPrevious<CR>")
-vim.keymap.set("n", "<leader>k", "<cmd>BufferNext<CR>")
-vim.keymap.set("n", "<leader>x", "<cmd>BufferClose<CR>")
-vim.keymap.set("n", "<leader>X", "<cmd>BufferClose!<CR>")
+    -- Set <space> as the leader key
+    -- See `:help mapleader`
+    --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+    vim.g.mapleader = " "
+    vim.g.maplocalleader = " "
 
--- Key mapping for opening and closing file tree
-vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
-vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>", { desc = "[T]oggle Nvim [T]ree" })
+    -- Keymappings for bar bar to switch and close buffers
+    vim.keymap.set("n", "<leader>j", "<cmd>BufferPrevious<CR>")
+    vim.keymap.set("n", "<leader>k", "<cmd>BufferNext<CR>")
+    vim.keymap.set("n", "<leader>x", "<cmd>BufferClose<CR>")
+    vim.keymap.set("n", "<leader>X", "<cmd>BufferClose!<CR>")
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
+    -- Key mapping for opening and closing file tree
+    vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
+    vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>", { desc = "[T]oggle Nvim [T]ree" })
 
--- [[ Setting options ]]
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+    -- Set to true if you have a Nerd Font installed and selected in the terminal
+    vim.g.have_nerd_font = true
 
--- Make line numbers default
-vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+    -- [[ Setting options ]]
+    -- See `:help vim.opt`
+    -- NOTE: You can change these options as you wish!
+    --  For more options, you can see `:help option-list`
 
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = "a"
+    -- Make line numbers default
+    vim.opt.number = true
+    -- You can also add relative line numbers, to help with jumping.
+    --  Experiment for yourself to see if you like it!
+    -- vim.opt.relativenumber = true
 
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
+    -- Enable mouse mode, can be useful for resizing splits for example!
+    vim.opt.mouse = "a"
 
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.schedule(function()
-	vim.opt.clipboard = "unnamedplus"
-end)
+    -- Don't show the mode, since it's already in the status line
+    vim.opt.showmode = false
 
--- Enable break indent
-vim.opt.breakindent = true
+    -- Sync clipboard between OS and Neovim.
+    --  Schedule the setting after `UiEnter` because it can increase startup-time.
+    --  Remove this option if you want your OS clipboard to remain independent.
+    --  See `:help 'clipboard'`
+    vim.schedule(function()
+        vim.opt.clipboard = "unnamedplus"
+    end)
 
--- Save undo history
-vim.opt.undofile = true
+    -- Enable break indent
+    vim.opt.breakindent = true
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+    -- Save undo history
+    vim.opt.undofile = true
 
--- Keep signcolumn on by default
-vim.opt.signcolumn = "yes"
+    -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+    vim.opt.ignorecase = true
+    vim.opt.smartcase = true
 
--- Decrease update time
-vim.opt.updatetime = 250
+    -- Keep signcolumn on by default
+    vim.opt.signcolumn = "yes"
 
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
+    -- Decrease update time
+    vim.opt.updatetime = 250
 
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+    -- Decrease mapped sequence wait time
+    -- Displays which-key popup sooner
+    vim.opt.timeoutlen = 300
 
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+    -- Configure how new splits should be opened
+    vim.opt.splitright = true
+    vim.opt.splitbelow = true
 
--- Preview substitutions live, as you type!
-vim.opt.inccommand = "split"
+    -- Sets how neovim will display certain whitespace characters in the editor.
+    --  See `:help 'list'`
+    --  and `:help 'listchars'`
+    vim.opt.list = true
+    vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
--- Show which line your cursor is on
-vim.opt.cursorline = true
+    -- Preview substitutions live, as you type!
+    vim.opt.inccommand = "split"
 
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+    -- Show which line your cursor is on
+    vim.opt.cursorline = true
 
--- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
+    -- Minimal number of screen lines to keep above and below the cursor.
+    vim.opt.scrolloff = 10
 
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+    -- [[ Basic Keymaps ]]
+    --  See `:help vim.keymap.set()`
 
--- Diagnostic keymaps
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+    -- Clear highlights on search when pressing <Esc> in normal mode
+    --  See `:help hlsearch`
+    vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+    -- Diagnostic keymaps
+    vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+    -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+    -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+    -- is not what someone will guess without a bit more experience.
+    --
+    -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+    -- or just use <C-\><C-n> to exit terminal mode
+    vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+    -- TIP: Disable arrow keys in normal mode
+    -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+    -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+    -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+    -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
+    -- Keybinds to make split navigation easier.
+    --  Use CTRL+<hjkl> to switch between windows
+    --
+    --  See `:help wincmd` for a list of all window commands
+    vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+    vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+    vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+    vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
+    -- [[ Basic Autocommands ]]
+    --  See `:help lua-guide-autocommands`
+
+    -- Highlight when yanking (copying) text
+    --  Try it with `yap` in normal mode
+    --  See `:help vim.highlight.on_yank()`
+    vim.api.nvim_create_autocmd("TextYankPost", {
+        desc = "Highlight when yanking (copying) text",
+        group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+        callback = function()
+            vim.highlight.on_yank()
+        end,
+    })
+
+end
+return M
