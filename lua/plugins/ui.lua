ca0aaf34c886e4e11763205d7aafa9de593ea238
Josh Lassman <lassmanjm@gmail.com>
2024-11-18 17:21:48 -0500

    add flags to conditionally enable each ui plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,58 +1,66 @@
-return {
-	{
-		"romgrk/barbar.nvim",
-		dependencies = {
-			"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
-			"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
-		},
-		init = function()
-			vim.g.barbar_auto_setup = false
-		end,
-		opts = {
-			-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-			-- animation = true,
-			-- insert_at_start = true,
-			-- …etc.
-		},
-		version = "^1.0.0", -- optional: only update when a new 1.x version is released
+return function(opts)
+	opts = opts or {}
 
-		config = function()
-			require("barbar").setup()
-			-- Keymappings for bar bar to switch and close buffers
-			vim.keymap.set("n", "<leader>j", "<cmd>BufferPrevious<CR>")
-			vim.keymap.set("n", "<leader>k", "<cmd>BufferNext<CR>")
-			vim.keymap.set("n", "<leader>x", "<cmd>BufferClose<CR>")
-			vim.keymap.set("n", "<leader>X", "<cmd>BufferClose!<CR>")
-		end,
-	},
-	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
+	return {
+		{
+			"romgrk/barbar.nvim",
+			dependencies = {
+				"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+				"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+			},
+			init = function()
+				vim.g.barbar_auto_setup = false
+			end,
+			opts = {
+				-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+				-- animation = true,
+				-- insert_at_start = true,
+				-- …etc.
+			},
+			version = "^1.0.0", -- optional: only update when a new 1.x version is released
+
+			config = function()
+				require("barbar").setup()
+				-- Keymappings for bar bar to switch and close buffers
+				vim.keymap.set("n", "<leader>j", "<cmd>BufferPrevious<CR>")
+				vim.keymap.set("n", "<leader>k", "<cmd>BufferNext<CR>")
+				vim.keymap.set("n", "<leader>x", "<cmd>BufferClose<CR>")
+				vim.keymap.set("n", "<leader>X", "<cmd>BufferClose!<CR>")
+			end,
+			cond = opts.disable_barbar,
 		},
-		config = function()
-			require("nvim-tree").setup({})
+		{
+			"nvim-tree/nvim-tree.lua",
+			version = "*",
+			lazy = false,
+			dependencies = {
+				"nvim-tree/nvim-web-devicons",
+			},
+			config = function()
+				require("nvim-tree").setup({})
 
-			-- Key mapping for opening and closing file tree
-			vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
-			vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>", { desc = "[T]oggle Nvim [T]ree" })
-		end,
-	},
-	{
-		"Mofiqul/vscode.nvim",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
-		config = function()
-			vim.cmd.colorscheme("vscode")
+				-- Key mapping for opening and closing file tree
+				vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
+				vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>", { desc = "[T]oggle Nvim [T]ree" })
+			end,
+			cond = opts.disable_nvim_tree,
+		},
+		{
+			"Mofiqul/vscode.nvim",
+			priority = 1000, -- Make sure to load this before all the other start plugins.
+			config = function()
+				vim.cmd.colorscheme("vscode")
 
-			vim.cmd.hi("Comment gui=none")
-		end,
-	},
-	{
-		"echasnovski/mini.statusline",
-		config = function()
-			require("mini.statusline").setup({})
-		end,
-	},
-}
+				vim.cmd.hi("Comment gui=none")
+			end,
+			cond = opts.disable_vscode_colorscheme,
+		},
+		{
+			"echasnovski/mini.statusline",
+			config = function()
+				require("mini.statusline").setup({})
+			end,
+			cond = opts.disable_mini_statusline,
+		},
+	}
+end
