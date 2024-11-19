50729e1c1e126b90868053d8098af564c82ce90f
lassmanjm <lassmanjm@gmail.com>
2024-11-19 12:38:07 -0500

    define api variable in nvim tree keymappins

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -40,6 +40,8 @@ return {
 				}
 			})
 
+			local api = require("nvim-tree.api")
+
 			-- Key mapping for opening and closing file tree
 			vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
 			vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>", { desc = "[T]oggle Nvim [T]ree" })
