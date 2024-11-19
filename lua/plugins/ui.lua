2072d77255e3d6d17b7428e34fea3ef812d651f7
lassmanjm <lassmanjm@gmail.com>
2024-11-19 13:03:58 -0500

    Remove failed nvim tree cd keymapping

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -45,10 +45,6 @@ return {
 			-- Key mapping for opening and closing file tree
 			vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
 			vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>", { desc = "[T]oggle Nvim [T]ree" })
-
-			-- Keymapping to cd into directory uder cursor (ctrl + Enter)
-			vim.keymap.set("n", "<leader>CR", api.tree.change_root_to_node, {desc = "Nvim Tree: CD"})
-
 		end,
 	},
 	{
