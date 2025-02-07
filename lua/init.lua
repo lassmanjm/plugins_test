3b9b296ec44b794d6848f90ee7c336dd7aa659db
lassmanjm <lassmanjm@gmail.com>
2025-02-06 22:28:33 -0500

    add a comment

--- lua/init.lua
+++ lua/init.lua
@@ -1,9 +1,9 @@
-M ={}
+M = {}
 
-M.setup = function(opts) 
-print("init.lua!!")
-    -- require("plugins/keymaps_and_options")
+M.setup = function(opts)
+	print("init.lua!!")
+	-- require("plugins/keymaps_and_options")
 end
+-- a comment
 
 return M
-
